5dbd79fdd47676004bfddafb64d98e47f9af7236
Brenenho <brenohenryck@gmail.com>
2023-04-16 20:23:19 -0300

    Js Finalizado

---
+++ script.js
@@ -0,0 +1,116 @@
+let mensagens = [];
+axios.defaults.headers.common['Authorization'] = '9TxfMaln8uP2z4em41wIu4rz';
+let usuario = prompt("Escreva o seu nome: ")
+
+function entrarnasala() {
+    const promessa = axios.post(
+        "https://mock-api.driven.com.br/api/vm/uol/participants",
+        {
+            name: usuario
+          }
+      );
+    
+        promessa.then(UsuarioCadastradaComSucesso);
+        promessa.catch(UsuarioErro);
+    }
+
+function UsuarioCadastradaComSucesso(deubom) {
+        console.log(deubom)
+    }
+
+function UsuarioErro(erro) {
+    while (erro.response.status === 400) {
+            usuario = prompt("Insira outro nome: ")
+            console.log(erro)
+        }
+    }
+  entrarnasala();
+
+  function manterconexao() {
+    const promessa = axios.post(
+        "https://mock-api.driven.com.br/api/vm/uol/status",
+        {
+            name: usuario
+          }
+      ); 
+      console.log(promessa)     
+  }
+
+setInterval(manterconexao, 5000)
+
+function limparInput() {
+    var getValue= document.querySelector(".escrever");
+        if (getValue.value !="") {
+            getValue.value = "";
+        }
+ }
+
+ const promise = axios.get('https://mock-api.driven.com.br/api/vm/uol/messages');
+ promise.then( processarListaRecebidaDoServidor ); 
+ 
+ // 3 - pegar a resposta e processar esta resposta ( renderizar no HTML a lista que o servidor me enviou)
+ function processarListaRecebidaDoServidor(res){
+     console.log(res);
+
+     mensagens = res.data;
+
+     console.log(mensagens.length)
+
+     console.log(mensagens)
+
+    renderizarMensagens();
+     
+ }
+
+function renderizarMensagens() {
+  
+  const mensagens1 = document.querySelector(".conteudo");
+
+  for(let i = 0; i < mensagens.length; i++){
+    let mensagem = mensagens[i];
+
+    
+    if (mensagem.type === "status") {
+      mensagens1.innerHTML += `
+    <div class="caixamensagem status">
+              <p><a class="horario">(${mensagem.time})</a>  <a class="negrito">${mensagem.from}</a> para <a class="negrito">${mensagem.to}:</a> ${mensagem.text}</p>
+            </div>
+    `
+    } else {
+      mensagens1.innerHTML += `
+    <div class="caixamensagem">
+              <p><a class="horario">(${mensagem.time})</a>  <a class="negrito">${mensagem.from}</a> para <a class="negrito">${mensagem.to}:</a> ${mensagem.text}</p>
+            </div>
+    `}
+   };
+ };
+  
+
+function inserirReceita() {
+    const mensagens = document.querySelector(".escrever").value;
+    
+    const promessa = axios.post(
+      "https://mock-api.driven.com.br/api/vm/uol/messages",
+      {
+        from: usuario,
+        to: "Todos",
+        text: mensagens,
+        type: "message" // ou "private_message" para o bônus
+    }
+    );
+  
+    promessa.then(enviosucesso);
+    limparInput();
+  }
+
+  function enviosucesso(mensagemenviada) {
+    
+    const promise = axios.get('https://mock-api.driven.com.br/api/vm/uol/messages');
+    promise.then( mensagemchegou );
+  }
+
+  function mensagemchegou(respostaatualizada) {
+
+    mensagens = respostaatualizada.data;
+    renderizarMensagens();
+  }
